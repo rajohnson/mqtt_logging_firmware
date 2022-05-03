4a0ce5474977e5d983d8aaa8a1c374a2e49f351a
Rob Johnson <rjohnson@dcs_inc.net>
2022-05-03 14:43:54 -0700

    connect to wifi

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import esp
-esp.check_fw()
+import network
+import credentials
+
+sta_if = network.WLAN(network.STA_IF)
+
+sta_if.active(True)
+sta_if.connect(credentials.ssid, credentials.password)
+
+while not sta_if.isconnected():  # wait to connect
+    pass
+
+print(sta_if.isconnected())
+print(sta_if.ifconfig())
