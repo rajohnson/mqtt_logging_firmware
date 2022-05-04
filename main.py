91f38ecdc460ea803e4e985987fa29fcf024bbe1
Rob Johnson <rjohnson@dcs_inc.net>
2022-05-03 18:27:12 -0700

    add mqtt - not working though. Cert problem.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import network
+from umqtt.simple import MQTTClient
+import ubinascii
 import credentials
+import machine
 
+# mqtt config variables
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+print("CLIENT_ID is {}".format(CLIENT_ID))
+
+# connect to wifi
 sta_if = network.WLAN(network.STA_IF)
 
 sta_if.active(True)
@@ -9,5 +17,27 @@ sta_if.connect(credentials.ssid, credentials.password)
 while not sta_if.isconnected():  # wait to connect
     pass
 
-print(sta_if.isconnected())
 print(sta_if.ifconfig())
+
+# mqtt pulication loop
+while True:
+    c = MQTTClient(
+           'esp8266_21195000',
+           credentials.mqtt_server,
+           port = 8883,
+           keepalive = 10000,
+           ssl = True,
+           ssl_params = {
+               'cert': credentials.cert,
+               'key': credentials.key
+               }
+           )
+    try:
+        c.connect()
+    except OSError:
+        print('error connecting to server')
+        machine.lightsleep(1000)
+        continue
+    c.publish(topic = 'test', msg = 'hi hi hi', qos = 0)
+    c.disconnect()
+    machine.lightsleep(1000)
